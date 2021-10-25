90264f2765ab156f592d6f6fa768c442ef286787
biancacomanescu97 <biancacomanescu97@gmail.com>
2021-10-25 18:43:05 +0100

    Added FLOW repository

--- src/Main.elm
+++ src/Main.elm
@@ -242,7 +242,7 @@ flow =
                     ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
                     in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
                     non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""" ]    
-              , div [ ] 
+              , a [ href "https://github.com/biancacomanescu97/flow", target "_blank" ]
                     [ button [ style "font-family" "Lucida Console, Courier, monospace" 
                              , style "font-size" "20px"
                              , style "position" "absolute"
